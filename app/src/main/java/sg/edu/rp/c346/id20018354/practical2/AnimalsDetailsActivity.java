1f30a1ad890e89215c0c878608c30cffb514b8cc
Hui-Sin <20018354@myrp.edu.sg>
2021-06-01 13:58:01 +0800

    Edited

--- app/src/main/java/sg/edu/rp/c346/id20018354/practical2/AnimalsDetailsActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/practical2/AnimalsDetailsActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,10 +15,12 @@ public class AnimalsDetailsActivity extends AppCompatActivity {
     TextView Names;
     TextView Diets;
     TextView CName;
+    TextView Characteristic;
     TextView SkinType;
     TextView LifeSpan;
     TextView Weights;
     TextView Link;
+    private float ourFontsize = 14f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,7 @@ public class AnimalsDetailsActivity extends AppCompatActivity {
         Names=findViewById(R.id.textViewAnimalName);
         Diets=findViewById(R.id.textViewDiet);
         CName=findViewById(R.id.textViewCommonName);
+        Characteristic=findViewById(R.id.textViewPhysicalCharacteristic);
         SkinType=findViewById(R.id.textViewSkinType);
         LifeSpan=findViewById(R.id.textViewLifespan);
         Weights=findViewById(R.id.textViewWeight);
@@ -38,6 +44,7 @@ public class AnimalsDetailsActivity extends AppCompatActivity {
         Names.setText(Name);
         Diets.setText(Diet);
         CName.setText(CommonName);
+        Characteristic.setText(Name +" Physical Characteristic");
         SkinType.setText(Skintypes);
         LifeSpan.setText(Lifespan);
         Weights.setText(Weight);
@@ -49,5 +56,44 @@ public class AnimalsDetailsActivity extends AppCompatActivity {
                 startActivity(intentView);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.increase) {
+            ourFontsize += 2f;
+            Names.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            Characteristic.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            Diets.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            CName.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            Weights.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            Link.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            LifeSpan.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            SkinType.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+
+            return true;
+        } else if (id == R.id.decrease) {
+            ourFontsize -= 2f;
+            Names.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            Characteristic.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            Diets.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            CName.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            Weights.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            Link.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            LifeSpan.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+            SkinType.setTextSize(TypedValue.COMPLEX_UNIT_SP, ourFontsize);
+
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
